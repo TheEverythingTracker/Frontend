7bba0af6b7653b3753cc2b6a2fb5940696e4b426
Hypnos8 <justsleeping22@gmail.com>
2023-05-04 18:53:48 +0200

    Added bounding box object mapping

---
+++ src/modules/BoundingBox.ts
@@ -0,0 +1,15 @@
+export class BoundingBox {
+     x : Number;
+     y: Number;
+     height: Number;
+     width: Number;
+     id!: string;
+     constructor(clickX : number, clickY : number, releaseX : number, releaseY : number) {
+        this.x =  clickX < releaseX ? clickX : releaseX;
+        this.y = clickY < releaseY ? clickY : releaseY;
+        
+        this.width  = Math.abs(releaseX - clickX) ;
+        this.height  = Math.abs(releaseY - clickY) ;
+
+     }
+}
