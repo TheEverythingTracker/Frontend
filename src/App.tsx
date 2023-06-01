b41674a947c0ce54e17c471fdd10508db6dfae72
Toudik Isavi <toudik.isavi@gmail.com>
2023-06-01 20:45:56 +0200

    wip synch boundarybox

--- src/App.tsx
+++ src/App.tsx
@@ -22,29 +22,61 @@ function App() {
 
     const boundingBoxesQueue = useRef(new Array());
 
+    //const intervalBoundingBox!: React.MutableRefObject<undefined>= useRef(undefined);
+
+    const handleIsStarted = useRef(false);
+
     const videoPlayerContext: VideoPlayerContext = new VideoPlayerContext(...useState<boolean>(false), ...useState<BoundingBox[]>([]));
 
     const WS_URL = 'ws://localhost:8765';
-
+/*
     React.useEffect(() => {
-        const intervalBoundingBox = setInterval(() => {
-            let boundingBoxData: BoundingBoxData | undefined = boundingBoxesQueue.current.shift();
-            console.log(Date.now() )
-            console.log(boundingBoxData)
+        console.log("rein in useeffekt");
+        console.log(boundingBoxesQueue.current.length);
+        if (boundingBoxesQueue.current.length > 0) {
 
-            if (boundingBoxData !== undefined) {
-                videoPlayerContext.setBoundingBoxes(boundingBoxData.boundingBoxes);
-                if (!videoPlayerContext.isPlaying) {
-                    let video = document.getElementById("video") as HTMLVideoElement;
-                    videoPlayerContext.setIsPlaying(true);
-                    video.play();
-                }
+            handleIsStarted.current = true;
+            console.log("rein in if abfrage");
+            if (!videoPlayerContext.isPlaying) {
+                let video = document.getElementById("video") as HTMLVideoElement;
+                videoPlayerContext.setIsPlaying(true);
+                video.play().then(result => setInterval(() => {
+
+                    let boundingBoxData: BoundingBoxData | undefined = boundingBoxesQueue.current.shift();
+
+                    if (boundingBoxData !== undefined) {
+                        videoPlayerContext.setBoundingBoxes(boundingBoxData.boundingBoxes);
+                    }
+
+                }, 100 / 3));
             }
-          }  , 100/3);
-          return () => clearInterval(intervalBoundingBox);
-    },[ ]);
 
-    
+            //return () => clearInterval(intervalBoundingBox.current);
+        }
+    },[boundingBoxesQueue.current]);
+*/
+    function test() {
+        if (boundingBoxesQueue.current.length > 0 && !handleIsStarted.current) {
+
+            handleIsStarted.current = true;
+            console.log("rein in if abfrage");
+            if (!videoPlayerContext.isPlaying) {
+                let video = document.getElementById("video") as HTMLVideoElement;
+                videoPlayerContext.setIsPlaying(true);
+                video.play().then(result => setInterval(() => {
+
+                    let boundingBoxData: BoundingBoxData | undefined = boundingBoxesQueue.current.shift();
+
+                    if (boundingBoxData !== undefined) {
+                        videoPlayerContext.setBoundingBoxes(boundingBoxData.boundingBoxes);
+                    }
+
+                }, 100/3));
+            }
+
+            //return () => clearInterval(intervalBoundingBox.current);
+        }
+    }
     
     const {sendMessage} = useWebSocket(WS_URL, {
         onOpen: () => {
@@ -56,6 +88,7 @@ function App() {
             if (jsonEvent.event_type === EventType.UPDATE_TRACKING) {
                 let updateEvent = jsonEvent as UpdateTrackingEvent;
                 boundingBoxesQueue.current.push(new BoundingBoxData(updateEvent.frame, updateEvent.bounding_boxes));
+                test();
             }
         }
     });
