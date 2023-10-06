5dd73de518e2f47620ea9589d3d3eec58cd30c4e
ninolus <23122368+ninolus@users.noreply.github.com>
2023-10-06 17:25:34 +0200

    add blacklist for deleted bounding-boxes and do not show them if on the list

--- src/models/VideoPlayerContext.ts
+++ src/models/VideoPlayerContext.ts
@@ -6,26 +6,32 @@ export class VideoPlayerContextData {
     setIsPlaying: Function;
     boundingBoxes: BoundingBox[];
     setBoundingBoxes: Function;
+    deletedBoundingBoxIds: number[];
+    setDeletedBoundingBoxIds: Function;
     boundingBoxListCleared: React.MutableRefObject<boolean>
     frameCounter: React.MutableRefObject<number> | null;
-    receivedFirstBox:React.MutableRefObject<boolean>;
+    receivedFirstBox: React.MutableRefObject<boolean>;
 
-/**
- * 
- * @param isPlaying: flag, true if video is currently played
- * @param setIsPlaying: setter for isPlaying-flag 
- * @param boundingBoxes: list of bounding boxes
- * @param setBoundingBoxes: setter for List of bounding boxes
- * @param boundingBoxListCleared: ref_variable indicating that the list of BoundingBoxes is empty because the user purposly deleted them
+    /**
+     * @param isPlaying {flag, true if video is currently played}
+     * @param setIsPlaying {setter for isPlaying-flag}
+     * @param boundingBoxes {list of bounding boxes}
+     * @param setBoundingBoxes {setter for List of bounding boxes}
+     * @param deletedBoundingBoxIds {blacklist for deleted bounding boxes, which should not be displayed anymore}
+     * @param setDeletedBoundingBoxIds {set blacklist}
+     * @param frameCounter {Counts the current frame number}
+     * @param boundingBoxListCleared {ref_variable indicating that the list of BoundingBoxes is empty because the user purposely deleted them}
+     * @param receivedFirstBox {indicates, that boundingbox-data has been received}
+     */
+    constructor(isPlaying: boolean, setIsPlaying: Function, boundingBoxes: BoundingBox[], setBoundingBoxes: Function, deletedBoundingBoxIds: number[], setDeletedBoundingBoxIds: Function,
+                frameCounter: React.MutableRefObject<number> | null, boundingBoxListCleared: React.MutableRefObject<boolean>, receivedFirstBox: React.MutableRefObject<boolean>) {
 
- */
-    constructor(isPlaying: boolean, setIsPlaying: Function, boundingBoxes: BoundingBox[], setBoundingBoxes: Function, 
-        frameCounter: React.MutableRefObject<number> | null, boundingBoxListCleared:React.MutableRefObject<boolean>,receivedFirstBox:React.MutableRefObject<boolean> ) {
-        
         this.isPlaying = isPlaying;
         this.setIsPlaying = setIsPlaying;
         this.boundingBoxes = boundingBoxes;
         this.setBoundingBoxes = setBoundingBoxes;
+        this.deletedBoundingBoxIds = deletedBoundingBoxIds;
+        this.setDeletedBoundingBoxIds = setDeletedBoundingBoxIds;
         this.frameCounter = frameCounter;
         this.boundingBoxListCleared = boundingBoxListCleared;
         this.receivedFirstBox = receivedFirstBox;
